39be33cadbc626ddc2e9d88aa4008789d83aeb8c
caue <caue19052003@gmail.com>
2025-04-01 19:56:22 -0300

    feat: styled components

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,10 @@
+export const defaultTheme = {
+  black: `#000`,
+  white: "#fff",
+
+  "beige-500": "#D29F80",
+  "beige-600": "#ab7c55",
+  "beige-700": "#6b3a21",
+
+  "olive-green-500": "#454a33"
+};
